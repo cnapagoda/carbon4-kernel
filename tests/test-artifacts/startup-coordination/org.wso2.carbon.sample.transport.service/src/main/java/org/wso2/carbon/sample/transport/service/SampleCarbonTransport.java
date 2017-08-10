bcd262e4583cc0633f73616e9f7e0bd608cda70e
cnapagoda <cnapagoda@gmail.com>
2017-08-10 09:39:17 +0530

    remove transport

--- tests/test-artifacts/startup-coordination/org.wso2.carbon.sample.transport.service/src/main/java/org/wso2/carbon/sample/transport/service/SampleCarbonTransport.java
+++ tests/test-artifacts/startup-coordination/org.wso2.carbon.sample.transport.service/src/main/java/org/wso2/carbon/sample/transport/service/SampleCarbonTransport.java
@@ -19,7 +19,6 @@ package org.wso2.carbon.sample.transport.service;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wso2.carbon.kernel.transports.CarbonTransport;
 
 /**
  * Sample CarbonTransport Service Component class.
